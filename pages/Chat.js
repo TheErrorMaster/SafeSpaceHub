48805c304c82f31d75547ecd6dc15c8730703ca0
Mauricio Macias <61210645+TheErrorMaster@users.noreply.github.com>
2023-09-25 21:09:58 -0700

    Update Chat.js

---
+++ pages/Chat.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export const ChatPage = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
