c71f99b26120b668b0950d256c5481753ca57053
Mauricio <mauricio.macias2535@gmail.com>
2023-10-03 00:12:32 -0700

    added gluestack

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
 import { TabNavigator } from './navigation/TabNavigator';
+import { config, GluestackUIProvider} from "@gluestack-ui/themed"
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TabNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <GluestackUIProvider config={config.theme}>
+          <TabNavigator />
+        </GluestackUIProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
