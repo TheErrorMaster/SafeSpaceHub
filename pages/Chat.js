4455c7794346ec57c50211ae808a1fae81334e91
Mauricio <mauricio.macias2535@gmail.com>
2023-10-24 22:23:52 -0700

    added ai to chat page

--- pages/Chat.js
+++ pages/Chat.js
@@ -1,22 +1,53 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
-// import { FlatList, ScrollView } from '@gluestack-ui/themed';
-import { Icon, ArrowUpIcon, ChevronRightIcon, ScrollView, Image, Input, InputField } from '@gluestack-ui/themed'
-import { chats } from '../utils';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Icon, ArrowUpIcon, ScrollView, Input, InputField } from '@gluestack-ui/themed'
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { useState } from 'react';
-import { Zocial } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
 
 
 export const ChatPage = ({ navigation }) => {
-  const messageColor = (obj) => { return (obj === 'bot' ? 'white' : 'lightblue') }
-  const messageDirection = (obj) => { return (obj === 'bot' ? 'flex-start' : 'flex-end') }
-  const [mess, setMess] = useState([])
+  const messageColor = (obj) => { return (obj === 'assistant' ? 'white' : 'lightblue') }
+  const messageDirection = (obj) => { return (obj === 'assistant' ? 'flex-start' : 'flex-end') }
+  const [mess, setMess] = useState([{ role: "system", content: "you are a medical bot that is ready to answer people's questions about sti and other stuff" }])
   const [input, setInput] = useState("")
-  const btn = () => {
+  const scrollViewRef = React.useRef();
+
+  const fetchChat = async () => {
+    try {
+      const OPENAI_KEY = ""; // ask me for the api
+      const URL = 'https://api.openai.com/v1/chat/completions';
+
+
+      const response = await fetch(URL, {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${OPENAI_KEY}`
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: mess,
+        })
+      });
+      const resp = await response.json();
+      setMess([...mess, resp?.choices[0]?.message]);
+    } catch (e) {
+      console.log('e', e);
+    }
+  };
+
+
+  useEffect(() => {
+    if (mess[mess.length - 1]?.role === 'user') {
+      fetchChat();
+    }
+  }, [mess])
+
+
+  const btn = async () => {
     try {
       if (input !== '') {
-        setMess([...mess, { user: 'maui', message: input }]);
+        setMess([...mess, { role: 'user', content: input }]);
+        setInput('');
       }
     } catch (e) {
       console.log('e', e);
@@ -24,16 +55,16 @@ export const ChatPage = ({ navigation }) => {
   }
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <TouchableOpacity style={{ paddingLeft: 10 }} onPress={() => navigation.pop()}>
-        <Ionicons name="arrow-back" size={30} color="black" />
-      </TouchableOpacity>
-      <ScrollView style={{ margin: 0, marginBottom: 70 }}>
+      <ScrollView style={{ margin: 0, marginBottom: 70 }}
+        ref={scrollViewRef}
+        onContentSizeChange={() => { () => scrollViewRef.current.scrollToEnd({ animated: true }) }}
+      >
         {
-          mess?.map(x => {
+          mess?.slice(1)?.map(x => {
             return (
-              <View style={{ flex: 1, padding: 5, alignItems: messageDirection(x?.user), shadowColor: '#171717', shadowOffset: { width: -2, height: 4 }, shadowOpacity: 0.2 }}>
-                <View style={{ backgroundColor: messageColor(x?.user), padding: 10, borderRadius: 5 }}>
-                  <Text>{x?.message}</Text>
+              <View style={{ flex: 1, padding: 5, alignItems: messageDirection(x?.role), shadowColor: '#171717', shadowOffset: { width: -2, height: 4 }, shadowOpacity: 0.2 }}>
+                <View style={{ backgroundColor: messageColor(x?.role), padding: 10, borderRadius: 5 }}>
+                  <Text>{x?.content}</Text>
                 </View>
               </View>
             )
@@ -46,7 +77,7 @@ export const ChatPage = ({ navigation }) => {
           size="sm"
           style={{ width: '90%', height: '100%', maxHeight: 50 }}
         >
-          <InputField placeholder='Send a message' multiline onChangeText={(a) => setInput(a)} />
+          <InputField placeholder='Send a message' multiline onChangeText={(a) => setInput(a)} value={input} />
         </Input>
         <TouchableOpacity style={{ paddingLeft: 10, justifyContent: 'center' }} onPress={btn}>
           <View style={{ flex: 1, width: 30, maxHeight: 30, borderRadius: 5, justifyContent: 'center', alignItems: 'center', paddingHorizontal: 10, backgroundColor: 'lightblue', borderRadius: 200 }}>
